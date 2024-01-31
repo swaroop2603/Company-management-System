a4d7c8c341ce653b49321bd4f589f70caea4780e
dev-referral <pswaroop2603@gmail.com>
2024-01-31 14:53:33 +0530

    updating databases

--- sequelize.js
+++ sequelize.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('company_management_system', 'root', '123', {
-  host: 'localhost',
+const sequelize = new Sequelize('sql6681003', 'sql6681003', 'CEf8UmpQi6', {
+  host: 'sql6.freesqldatabase.com',
   dialect: 'mysql',
 });
-
 module.exports = sequelize;
+  
